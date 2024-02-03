2fc17b43cedd047434644d654c15263ee37d6606
Danil Skurikhin <daskurikhin@ozon.ru>
2024-02-03 23:39:23 +0500

    task2: clear function

--- day-2/task2.js
+++ day-2/task2.js
@@ -1,34 +1,13 @@
 const obj = {};
 
-setByPath(obj, 'foo.bar', 1);
-setByPath(obj, 'foo.bla', 2);
-setByPath(obj, 'baz.foo.bla', 'AAAA!!!');
-setByPath(obj, '.', () => console.log('fnc'));
-setByPath(obj, '', '2222!!!');
-setByPath(obj, 222, '2222!!!');
-setByPath(2, 'baz.foo.bla', '2222!!!');
-setByPath(2, 'baz.foo.bla', '2222!!!');
+const setByPath2 = (obj, path, value) => path
+    .split('.')
+    .reduce((acc, key, id, arr) => id !== arr.length - 1 ? acc[key] ??= {} : acc[key] = value, obj)
+
+setByPath2(obj, 'foo.bar.baz', 1);
+setByPath2(obj, 'foo.bar', 1);
+setByPath2(obj, 'foo.bla', 2);
+setByPath2(obj, 'baz.foo.bla', 'AAAA!!!');
+setByPath2(2, 'baz.foo.bla', '2222!!!');
 
 console.dir(obj); // {foo: {bar: 1, bla: 2}}
-
-function setByPath(obj, path, value) {
-    // проверки. Почти полностью можно исключить тайпскриптом
-    if (!path) return // throw new Error('Путь не может быть пустым')
-    if (typeof path !== 'string') return // throw new Error('Второй аргумент (путь) должен быть строкой')
-    if (typeof obj !== 'object') return // throw new Error('Первый аргумент должен быть объектом')
-
-    const extractRegex = /([^\.]+)\.|(.+)$/ // возможно не самая красивая регулярка
-    const key = path.match(extractRegex)[1] || path.match(extractRegex)[2] // забираем первую или вторую скобочную группу
-    const rest = path.replace(extractRegex, '') // выцарапываем оставшуюся строку
-
-    if (!rest) {
-        // получается это последний ключ. записываем значение
-        obj[key] = value
-    } else {
-        if (!obj.hasOwnProperty(key)) {
-            // если нет такого ключа записываем новый объект в это поле
-            obj[key] = {}
-        }
-        setByPath(obj[key], rest, value) // погружаемся внутрь
-    }
-}
