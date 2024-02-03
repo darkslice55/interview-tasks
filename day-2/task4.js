7f5adc182e5665e11f7e84ddfad6135bc2d6830a
Danil Skurikhin <daskurikhin@ozon.ru>
2024-02-03 19:49:50 +0500

    task4: add base checking

--- day-2/task4.js
+++ day-2/task4.js
@@ -1,11 +1,20 @@
 const MAIN_BASE = 10
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const hexPrefix = '0x'
 
-const myParseInt = function(input, base = MAIN_BASE) {
-    const overDecimal = base - MAIN_BASE
-    const regexPattern = new RegExp(`^\-?(0x)?[\\d${overDecimal > 0 ? `A-${alphabet[overDecimal - 1]}`: ''}]+`)
-    const value = input.match(regexPattern)?.[0]
-    return value ? convertToDecimal(value, base) : NaN
+const myParseInt = function(input, base) {
+    try {
+        const str = String(input)
+        const checkedBase = checkBase(base)
+    
+        const overDecimal = checkedBase - MAIN_BASE
+        const regexPattern = new RegExp(`^\-?(${hexPrefix})?[\\d${overDecimal > 0 ? `A-${alphabet[overDecimal - 1]}`: ''}]+`)
+        const value = str.match(regexPattern)?.[0].replace(hexPrefix,'')
+        if (!value) throw new Error()
+        return convertToDecimal(value, checkedBase)
+    }catch(err) {
+        return NaN
+    }
 }
 
 // base: <= 36
@@ -21,7 +30,11 @@ const convertToDecimal = function(input, base) {
         result += curNumber * base**(str.length - 1 - i)
     }
     return result * (isNegative ? -1 : 1)
+}
 
+const checkBase = function(base) {
+    if (!base || typeof base !== 'number') return MAIN_BASE
+    return String(base).split('.')[0]
 }
 
 console.log(myParseInt('10'));      // 10
@@ -29,22 +42,16 @@ console.log(myParseInt('-10', 2));  // -2
 console.log(myParseInt('FFP', 16)); // 255
 console.log(myParseInt('--20'));    // NaN
 console.log(myParseInt('asd213123asd'));    // NaN
-console.log(parseInt('asd213123asd'));    // NaN
-
-// console.log(myParseInt('10') === parseInt('10')); 
-// console.log(myParseInt('-10', 2) === parseInt('-10', 2));
-// console.log(myParseInt('FFP', 16) === parseInt('FFP', 16));
-// console.log(myParseInt('--20') === parseInt('--20')); 
-// console.log(myParseInt('213123asd') === parseInt('213123asd'));  
-// console.log(myParseInt('asd213123asd') === parseInt('asd213123asd'));  
-console.log(myParseInt("0xF", 16));
-console.log(myParseInt("F", 16));
-console.log(myParseInt("17", 8));
-console.log(myParseInt("015", 10)); // but `myParseInt('015', 8)` will return 13
-console.log(myParseInt("15,123", 10));
-console.log(myParseInt("FXX123", 16));
-console.log(myParseInt("1111", 2));
-console.log(myParseInt("15 * 3", 10));
-console.log(myParseInt("15e2", 10));
-console.log(myParseInt("15px", 10));
-console.log(myParseInt("12", 13));
+console.log(parseInt('123sa', 'asdasd'));    // 123
+console.log(parseInt('123sa', 10.3));    // 123
+console.log(myParseInt("0xF", 16)); // 15
+console.log(myParseInt("F", 16)); // 15
+console.log(myParseInt("17", 8)); // 15
+console.log(myParseInt("015", 10)); // 15
+console.log(myParseInt("15,123", 10)); // 15
+console.log(myParseInt("FXX123", 16)); // 15
+console.log(myParseInt("1111", 2)); // 15
+console.log(myParseInt("15 * 3", 10)); // 15
+console.log(myParseInt("15e2", 10)); // 15
+console.log(myParseInt("15px", 10)); // 15
+console.log(myParseInt("12", 13)); // 15
