0f83b1049bf2c260d2bd98d808c8ac90247c707b
Danil Skurikhin <daskurikhin@ozon.ru>
2024-02-09 09:47:42 +0500

    [day-7]: task-3& Add number resolver

--- day-7/task3.js
+++ day-7/task3.js
@@ -1,6 +1,7 @@
 console.log(dasherize('createDocumentFragment')); // 'create-document-fragment'
 console.log(dasherize('SuperMAN'));               // 'super-man'
 console.log(dasherize('VirtualDOMFragment'));     // 'virtual-dom-fragment'
+console.log(dasherize('Virtual131DOM134'));     // 'virtual131-dom134'
 
 
 function dasherize(str) {
@@ -12,21 +13,33 @@ function dasherize(str) {
         upperCharsCount++
     }
 
+    function isNotUpper(char) {
+        return char !== char.toUpperCase()
+    }
+
+    function isNumber(char) {
+        return !Number.isNaN(Number(char))
+    }
+
     for (let i = 1; i < str.length; i++) {
         const char = str.charAt(i)
-        if (char !== char.toUpperCase()) continue
+        if (isNotUpper(char) || isNumber(char)) continue
 
         const startPosition = i
         let endPosition = startPosition
         
         while (true) {
-            i++
-            const nextChar = str.charAt(i)
-            if (nextChar !== nextChar.toUpperCase()) {
-                endPosition = i - 1
+            if (i >= str.length) {
                 break
             }
-            if (i >= str.length) {
+
+            i++
+            const nextChar = str.charAt(i)
+
+            if (isNumber(nextChar)) continue
+            
+            if (isNotUpper(nextChar)) {
+                endPosition = i - 1
                 break
             }
         }
